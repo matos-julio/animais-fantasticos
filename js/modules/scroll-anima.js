c74d5801b37726c4a7b58cadc19957a55239e277
Júlio <juliomcavalcanti@yahoo.com.br>
2021-11-17 19:43:48 -0300

    animacao no scroll refatorado com class

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,26 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top; // pega o topo do elemento
+      const isSectionVisible = sectionTop - this.windowMetade < 0; // se o elemento esta aparecendo na tela
+
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo");
+      }
+    });
+  }
+
+  init() {
+    this.animaScroll();
+    window.addEventListener("scroll", this.animaScroll);
+  }
+}
