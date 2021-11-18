81024851a7cdfbe7ec3a8d0e3090d2cd516e2959
Júlio <juliomcavalcanti@yahoo.com.br>
2021-11-17 22:20:20 -0300

    menu mobile refatorado

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import ScrollAnima from "./modules/scroll-anima.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
+import MenuMobile from "./modules/menu-mobile.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
 
-import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
@@ -42,8 +42,10 @@ const dropdownMenu = new DropdownMenu("[data-dropdown]", [
 ]);
 dropdownMenu.init();
 
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
+initFuncionamento();
+
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
-
-initMenuMobile();
-initFuncionamento();
